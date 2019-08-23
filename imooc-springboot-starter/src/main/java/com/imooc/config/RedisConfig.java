529b2f38fa5bde851eed41b3f61f55e0c06bfbea
yang123456 <763216917@qq.com>
2019-08-23 21:38:26 +0800

    imooc springboot

---
+++ imooc-springboot-starter/src/main/java/com/imooc/config/RedisConfig.java
@@ -0,0 +1,10 @@
+package com.imooc.config;
+
+public class RedisConfig {
+	public static void main(String[] args) {
+		System.out.println("hello git13134");
+		System.out.println("hello git13134");
+		System.out.println("hello git13134");
+
+	}
+}
