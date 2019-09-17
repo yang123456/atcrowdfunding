03edabf21e8c2f38a2a79528b8feef3f03f65434
yang123456 <763216917@qq.com>
2019-09-17 17:44:01 +0800

    sql

--- atcrowdfunding-web-mvc/sql/atcrowdfunding-v.sql
+++ atcrowdfunding-web-mvc/sql/atcrowdfunding-v.sql
@@ -205,3 +205,16 @@ CREATE TABLE `user` (
 -- ----------------------------
 INSERT INTO `user` VALUES ('1', 'zhangsan', '12');
 INSERT INTO `user` VALUES ('2', '啊啊啊', '12');
+
+
+-- ----------------------------
+-- Table structure for editor
+-- ----------------------------
+DROP TABLE IF EXISTS `editor`;
+CREATE TABLE `editor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text_content` longtext,
+  `content` longtext,
+  `create_time` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
